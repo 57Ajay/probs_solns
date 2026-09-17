e0a5850348cf1c48f65d4bb020a785d93cdf83fc
57ajay <57ajay.u@gmail.com>
2026-09-17 18:55:23 +0530

    optemized Traffic Lights solution
    Signed-off-by: 57ajay <57ajay.u@gmail.com>

--- src/cses/sorting_and_searching/traffic_lights.rs
+++ src/cses/sorting_and_searching/traffic_lights.rs
@@ -29,60 +29,32 @@ pub fn main() {
         None => return,
     };
 
-    let mut btset = BTreeSet::<(usize, usize)>::new();
+    let mut btset = BTreeSet::<usize>::new();
     let mut btmap = BTreeMap::<usize, usize>::new();
-    let v1 = iter.next().unwrap().parse::<usize>().unwrap();
-    btset.insert((0, v1));
-    btset.insert((v1, x));
 
-    update_count(&mut btmap, v1, UCOUNT::INC);
-    update_count(&mut btmap, x - v1, UCOUNT::INC);
+    btset.insert(0);
+    btset.insert(x);
+    btmap.insert(x, 1);
 
-    write!(writer, "{} ", btmap.keys().next_back().unwrap()).unwrap();
-
-    for _ in 1..n {
+    for _ in 0..n {
         let val = iter.next().unwrap().parse::<usize>().unwrap();
 
-        if let Some(&x) = btset.range(..(val, 0)).next_back() {
-            btset.remove(&x);
-            btset.insert((x.0, val));
-            btset.insert((val, x.1));
-            let b = x.1 - x.0;
-            update_count(&mut btmap, b, UCOUNT::DEC);
+        let start = *btset.range(..val).next_back().unwrap();
+        let end = *btset.range(val..).next().unwrap();
 
-            let d1 = val - x.0;
-            let d2 = x.1 - val;
-            update_count(&mut btmap, d1, UCOUNT::INC);
-            update_count(&mut btmap, d2, UCOUNT::INC);
-
-            write!(writer, "{} ", btmap.keys().next_back().unwrap()).unwrap();
+        btset.insert(val);
+        if let std::collections::btree_map::Entry::Occupied(mut e) = btmap.entry(end - start) {
+            if *e.get() == 1 {
+                e.remove();
+            } else {
+                *e.get_mut() -= 1;
+            }
         }
+
+        *btmap.entry(val - start).or_insert(0) += 1;
+        *btmap.entry(end - val).or_insert(0) += 1;
+        write!(writer, "{} ", btmap.keys().next_back().unwrap()).unwrap();
     }
 
     writer.flush().unwrap();
-}
-
-enum UCOUNT {
-    DEC,
-    INC,
-}
-
-fn update_count(map: &mut BTreeMap<usize, usize>, val: usize, op: UCOUNT) {
-    match map.get_mut(&val) {
-        Some(count) => match op {
-            UCOUNT::DEC => {
-                if *count <= 1 {
-                    map.remove(&val);
-                } else {
-                    *count -= 1;
-                }
-            }
-            UCOUNT::INC => {
-                *count += 1;
-            }
-        },
-        None => {
-            map.insert(val, 1);
-        }
-    }
 }
